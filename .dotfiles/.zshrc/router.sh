380d0b4450445ad4626c041890e3c45e4873107b
Henrik L <helagro@outlook.com>
2024-03-30 11:25:09 +0100

    Improve bootstrap

--- .dotfiles/.zshrc/router.sh
+++ .dotfiles/.zshrc/router.sh
@@ -1,6 +1,5 @@
 source "$HOME/.dotfiles/.zshrc/shared.sh"
 source "$HOME/.dotfiles/.zshrc/secrets.sh"
-source "$HOME/.dotfiles/.zshrc/zshOpt.zsh"
 
 if [ "$(uname)" = "Darwin" ]; then
     source "$HOME/.dotfiles/.zshrc/macOS.sh"
--- .dotfiles/setup/main.sh
+++ .dotfiles/setup/main.sh
@@ -1,2 +1,18 @@
-sourceLine="source \"$HOME/.dotfiles/.zshrc/router.sh\""
-grep -qF "$sourceLine" "$HOME/.zshrc" || echo "$sourceLine" >>"$HOME/.zshrc"
+cd $HOME
+
+function addIfMissing {
+    grep -qF "$1" "$HOME/.zshrc" || echo "$1" >>"$HOME/.zshrc"
+}
+
+# ----------------- ADD MY ZSH CONFIGURATION ----------------- #
+
+addIfMissing 'source "$HOME/.dotfiles/.zshrc/router.sh"'
+
+# ----------------------- ADD OH MY ZSH ---------------------- #
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    git clone git@github.com:helagro/ohmyzsh.git $HOME/.oh-my-zsh
+fi
+
+addIfMissing 'export ZSH="$HOME/.oh-my-zsh"'
+addIfMissing 'source "$ZSH/oh-my-zsh.sh"'
