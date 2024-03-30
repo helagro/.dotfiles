666c7973b5d1c9f64465f72e2cc4c7db75126030
Henrik L <helagro@outlook.com>
2024-03-30 15:14:05 +0100

    Add install brew

--- .dotfiles/setup/macOS/other.sh
+++ .dotfiles/setup/macOS/other.sh
@@ -2,3 +2,7 @@ if [ -d "$HOME/.iterm2" ]; then
     echo "Setting iTerm preference folder"
     defaults write com.googlecode.iterm2 PrefsCustomFolder "$HOME/.dotfiles"
 fi
+
+if ! command -v brew &>/dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
