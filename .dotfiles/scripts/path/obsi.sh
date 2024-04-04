a44e3adced2bf1e89557b5d4b51460733826daa5
Henrik L <helagro@outlook.com>
2024-04-04 20:02:52 +0200

    Unspecified

--- .dotfiles/scripts/path/obsi.sh
+++ .dotfiles/scripts/path/obsi.sh
@@ -1,11 +1,10 @@
 vault="/Users/h/Dropbox/Apps/remotely-save/vault"
-doc="/Users/h/Documents"
-scripts="$doc/scripts/macOS"
+scripts="$HOME/.dotfiles/scripts/"
 
 [[ -z "$1" ]] && 1="0"
 
-find $vault/i -type f -name "*.md" | while IFS= read -r line; do
-    items=$($scripts/build/obsi "$line")
+find $vault/i -maxdepth 1 -type f -name "*.md" | while IFS= read -r line; do
+    items=$($scripts/obsi "$line")
 
     if [[ $items -ge $1 ]]; then
         echo $line : $items
