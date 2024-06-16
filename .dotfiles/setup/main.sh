cb92b26c3cfebbef6ac89af9b04edbb60f9f25fc
Henrik L <helagro@outlook.com>
2024-06-16 16:22:35 +0200

    Unspecified

--- .dotfiles/setup/main.sh
+++ .dotfiles/setup/main.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 cd $HOME
+yadm -C $HOME/.dotfiles remote set-url origin git@github.com:helagro/.dotfiles.git
 
 function addIfMissing {
     grep -qF "$1" "$HOME/.zshrc" || echo "$1" >>"$HOME/.zshrc"
@@ -13,7 +14,8 @@ addIfMissing 'source "$HOME/.dotfiles/.zshrc/router.sh"'
 # ----------------------- ADD OH MY ZSH ---------------------- #
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    git clone git@github.com:helagro/ohmyzsh.git $HOME/.oh-my-zsh
+    git clone https://github.com/helagro/ohmyzsh.git $HOME/.oh-my-zsh
+    git -C $HOME/.oh-my-zsh remote set-url origin git@github.com:helagro/ohmyzsh.git
 fi
 
 addIfMissing 'export ZSH="$HOME/.oh-my-zsh"'
