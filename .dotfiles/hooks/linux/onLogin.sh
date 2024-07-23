33a65ed1e2502f41f04f0f6ff716d40e5d5be42a
Henrik L <helagro@outlook.com>
2024-07-23 15:27:43 +0000

    Msg:

--- .dotfiles/hooks/linux/onLogin.sh
+++ .dotfiles/hooks/linux/onLogin.sh
@@ -1,3 +1,14 @@
+#!/bin/zsh
+
+source $HOME/.dotfiles/.zshrc/secrets.sh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 cd $HOME
 python3 dropbox.py start
-pm2 start online-tools resume todoist-app
+
+# echo "Starting services..."
+# pm2 start online-tools resume todoist-app
+
+echo "Done!"
