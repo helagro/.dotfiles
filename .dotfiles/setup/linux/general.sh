3dce95720b7bbadcd6cc93f70e5f5250883e00cf
Henrik L <helagro@outlook.com>
2024-07-23 15:48:52 +0000

    Msg: Fix linux bugs

--- .dotfiles/setup/linux/general.sh
+++ .dotfiles/setup/linux/general.sh
@@ -1,4 +1,9 @@
 mkdir -p ~/.config/systemd/user
-cp -f $HOME/.dotfiles/setup/linux/dotfiles.service /etc/systemd/system/dotfiles.service
 
-su -c "sudo systemctl daemon-reload && sudo systemctl enable dotfiles.service && sudo systemctl start dotfiles.service"
+su -c "
+cp -f $HOME/.dotfiles/setup/linux/dotfiles.service /etc/systemd/system/dotfiles.service && \
+sudo systemctl daemon-reload && \
+sudo systemctl enable dotfiles.service && \
+sudo systemctl start dotfiles.service
+"
+
