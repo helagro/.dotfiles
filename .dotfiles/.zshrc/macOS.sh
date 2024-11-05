e8653d3f69f4b61877ef5b08cb792f1a54a71292
Henrik L <helagro@outlook.com>
2024-11-05 20:41:36 +0100

    Add talk function

--- .dotfiles/.zshrc/macOS.sh
+++ .dotfiles/.zshrc/macOS.sh
@@ -12,6 +12,7 @@ fi
 
 export PATH="/Users/h/Library/Python/3.9/bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH=$PATH:$HOME/go/bin
 
 if command -v rbenv &>/dev/null; then
     eval "$(rbenv init -)"
@@ -24,6 +25,31 @@ alias archive="$HOME/Documents/archiver-go/build/macOS"
 alias breake="nvim $doc/break-timer/.env"
 
 # ------------------------- OTHER FUNCTIONS ------------------------ #
+
+function talk {
+    local text
+    read -r -d '' text
+
+    local chunk_size=4500
+    local text_length=${#text}
+    local start=1
+    local files=()
+    local file_name=""
+
+    while ((start <= text_length)); do
+        local chunk="${text[start - 1, start + chunk_size - 2]}"
+
+        file_name="$HOME/Desktop/$(cnt | tr -d '[:space:]').mp3"
+        echo "$chunk" | gosling - $file_name -r 1.2
+        files+=("$file_name")
+        ((start += chunk_size))
+    done
+
+    file_name="$HOME/Desktop/$(cnt | tr -d '[:space:]').mp3"
+    cat "${files[@]}" >"$file_name"
+    open $file_name
+    rm "${files[@]}"
+}
 
 function pass {
     local passw=$(op item get "$@" --reveal --fields password)
